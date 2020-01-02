5cbb7e2de58e1116d45f0929e643b249d195a2cb
Adrian <adrianjleong@gmail.com>
2020-01-02 18:59:36 -0300

    comienzo de asignacion de tickets

--- app.py
+++ app.py
@@ -1,56 +1,27 @@
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
-
-chrome_driver = webdriver.Chrome(r'P:\proyectos-pycharm\drivers\chrome\chromedriver.exe')
-#chrome_driver2 = webdriver.Chrome(r'P:\proyectos-pycharm\drivers\chrome\chromedriver.exe')
-#firefox_driver = webdriver.Chrome(r'P:\proyectos-pycharm\drivers\firefox\geckodriver.exe')
-
-url =  'https://www.seleniumeasy.com/test/basic-first-form-demo.html'
-url2 =  'https://www.google.com'
-
-#chrome_driver.maximize_window()
-chrome_driver.get(url)
-text_area = chrome_driver.find_element_by_id('user-message')
-show_message_button = chrome_driver.find_element_by_class_name('btn-default')
+from funtions import openFirefoxWebs, openChromePhone
+from dashlet import Dashlet
+from database import webs
 
 
 
-##print(text_area.get_attribute('innerHTML'))
+#CHORME PART
+chrome_options = webdriver.ChromeOptions()
+prefs = {"profile.default_content_setting_values.notifications" : 2}
+chrome_options.add_experimental_option("prefs",prefs)
+chrome_driver = webdriver.Chrome(options=chrome_options, executable_path=r'P:\proyectos-pycharm\drivers\chrome\chromedriver.exe')
+openChromePhone(chrome_driver, webs[5])
 
-#assert 'Enter your messaget' in chrome_driver.page_source
-
-text_area.clear()
-text_area.send_keys('I am extra cool')
-
-show_message_button.click()
-
-message_showed = chrome_driver.find_element_by_id('display')
-
-print(message_showed.get_attribute('innerHTML'))
-
-chrome_driver.implicitly_wait(3)
-
-# NEW TAB chrome_driver.execute_script("window.open('https://www.google.com');")
-
-# Open a new window
-chrome_driver.execute_script("window.open('');")
-
-chrome_driver.switch_to.window(chrome_driver.window_handles[1])
-chrome_driver.get(url2)
-
-# Close the tab with URL B
-chrome_driver.close()
-
-chrome_driver.switch_to.window(chrome_driver.window_handles[0])
-print("Current Page Title is : %s" %chrome_driver.title)
+#FIREFOX PART
+firefox_driver = webdriver.Firefox(executable_path=r'P:\proyectos-pycharm\drivers\firefox\geckodriver.exe')
+openFirefoxWebs(firefox_driver, webs)
+firefox_driver.switch_to.window(firefox_driver.window_handles[0])
 
 
+#TICKET MANAGEMENT
 
+dashlet = Dashlet(firefox_driver)
 
+dashlet.get_all_results()
 
-
-
-
-
-
-
+print(dashlet.tickets)
